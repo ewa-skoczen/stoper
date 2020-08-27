4e5a46dff87cb10670395290ba2c7daac3959c04
ewwwa <ewa-skoczen@outlook.com>
2020-08-27 19:54:52 +0200

    Add .toFixed(2)

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const startBtn = document.querySelector('.start');
 const resetBtn = document.querySelector('.reset');
 
 const start = () => {
-    // startBtn.classList.toggle('started');
     startBtn.classList.contains('started') ? startBtn.classList.remove('started') : startBtn.classList.add('started');
     console.log(startBtn.classList)
     const timer = document.querySelector('.time div');
@@ -12,12 +11,12 @@ const start = () => {
         let currentTime = (new Date().getTime());
         let interval = setInterval(() => {
             currentTime = (new Date().getTime());
-            timer.textContent = Math.round((currentTime - startTime) / 10) / 100;
+            timer.textContent = (Math.round((currentTime - startTime) / 10) / 100).toFixed(2);
             if (!(startBtn.classList.contains('started'))) clearInterval(interval);
         }, 100);
         resetBtn.addEventListener('click', () => {
-            clearInterval(interval);
             startBtn.classList.remove('started');
+            clearInterval(interval);
             startBtn.textContent = 'start';
             timer.textContent = '---';
         });
